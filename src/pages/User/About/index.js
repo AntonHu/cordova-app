780cd253469d41f50b974d700c17c0c42d09649e
AntonHu <504134526y@qq.com>
2018-07-31 14:59:59 +0800

    打包0.0.6

--- src/pages/User/About/index.js
+++ src/pages/User/About/index.js
@@ -99,7 +99,7 @@ class About extends React.PureComponent {
   state = {
     checking: false,
     checkText: '正在检查更新...',
-    appVersion: '0.0.5'
+    appVersion: '0.0.6'
   };
 
   onClick = (v) => {
