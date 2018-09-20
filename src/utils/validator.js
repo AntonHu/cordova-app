5c6fb097b131bdd54069d5c877b9e56ce46b9212
AntonHu <504134526y@qq.com>
2018-09-20 17:29:28 +0800

    书写validator类的test
    Differential Revision: http://cr.thundersdata.com:26170/D956

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -6,7 +6,7 @@ import { type } from './validate';
 class Validator {
   constructor(strategies) {
     this.cache = []; //保存校验规则
-    this.strategies = strategies; //每个实例自身需要的校验规则
+    this.strategies = strategies || {}; //每个实例自身需要的校验规则
   }
   add(value, rules) {
     for (let rule of rules) {
@@ -21,11 +21,11 @@ class Validator {
         } else {
           return (() => {
             console.error(
-              `strategy must be function, but got ${type(
+              `strategy必须是一个函数, 但是得到了${type(
                 this.strategies[strategy]
-              )},please check your strategy.`
+              )},请检查你得strategy.`
             );
-          }).apply(value, strategyAry);
+          })();
         }
       });
     }
@@ -34,10 +34,11 @@ class Validator {
     for (let validatorFunc of this.cache) {
       let errorMsg = validatorFunc(); //开始校验，并取得校验后的返回信息
       if (errorMsg) {
-        //如果有确切返回值，说明校验没有通过
         return errorMsg;
       }
     }
+    //直接运行start表示不校验，默认校验通过返回空字符串，不使用undefined
+    return '';
   }
 }
 
